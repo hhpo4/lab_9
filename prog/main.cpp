5bc770e7ccd7a7edd0e36eab12377908e250e5af
Nikita Kolymazhnov <nikita.kolymazhnov@gmail.com>
2024-12-19 21:29:24 +0300

    3.1

--- prog/main.cpp
+++ prog/main.cpp
@@ -10,5 +10,6 @@ int main(void)
     cin >> a >> b;
     cout << a << " + " << b << " = " << a + b << endl;
     cout << a << " + " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
     return 0;
 }
