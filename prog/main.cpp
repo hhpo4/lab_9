cd1e5d7bdd89633eb5af922f53650898d82f0d32
Nikita Kolymazhnov <nikita.kolymazhnov@gmail.com>
2024-12-19 21:01:49 +0300

    2.5.1

--- prog/main.cpp
+++ prog/main.cpp
@@ -7,5 +7,6 @@ int main(void)
     cout << "Enter two numbers: ";
     cin >> a >> b;
     cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " + " << b << " = " << a - b << endl;
     return 0;
 }
