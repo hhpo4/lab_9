1e1df00781711211bb06bb5d8794ac65fb45c05d
Nikita Kolymazhnov <nikita.kolymazhnov@gmail.com>
2024-12-19 20:51:59 +0300

    git init

---
+++ prog/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    return 0;
+}
